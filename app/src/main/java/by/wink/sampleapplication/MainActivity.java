cb1345405f12956b74842331819e4caa4b7897b0
Amine Nokra <amine.nokra@gmail.com>
2017-01-25 16:46:24 +0100

    first commit

---
+++ app/src/main/java/by/wink/sampleapplication/MainActivity.java
@@ -0,0 +1,50 @@
+package by.wink.sampleapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by amine on 25/01/17.
+ */
+
+public class MainActivity extends Activity {
+
+
+    public static final String USERNAME_KEY = "username";
+
+    TextView welcomeTv;
+    Button changeTextBtn;
+    EditText changeTextEdit;
+    MainActivity activity = this;
+    String username;
+
+    View.OnClickListener changeTxtBtnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            username = changeTextEdit.getText().toString();
+            Intent intent = new Intent(activity,SecondActivity.class);
+            intent.putExtra(USERNAME_KEY,username);
+            intent.putExtra("age",18);
+            startActivity(intent);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        welcomeTv = (TextView) findViewById(R.id.welcome_tv);
+        changeTextBtn = (Button) findViewById(R.id.change_text_btn);
+        changeTextEdit = (EditText)findViewById(R.id.change_text_edit);
+        changeTextBtn.setOnClickListener(changeTxtBtnListener);
+
+    }
+
+
+
+}
---
+++ app/src/main/java/by/wink/sampleapplication/SecondActivity.java
@@ -0,0 +1,25 @@
+package by.wink.sampleapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends Activity {
+
+    TextView nameTV;
+    Intent intent;
+    String username;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        nameTV = (TextView)findViewById(R.id.name_tv);
+        intent = getIntent();
+        username = intent.getStringExtra(MainActivity.USERNAME_KEY);
+        nameTV.setText(username);
+
+    }
+}
